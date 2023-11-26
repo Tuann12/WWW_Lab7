764b0e8fcaffce8e079da7e61a1d001d6dad408c
Nguyen Thanh Tuan <ngthtuan333@gmail.com>
2023-11-26 17:31:47 +0700

    Upload project

---
+++ src/main/java/vn/edu/iuh/fit/Week7LabNguyenThanhTuan20040861Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week7LabNguyenThanhTuan20040861Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week7LabNguyenThanhTuan20040861Application.class, args);
+    }
+
+}
